45dc33df2e7e3783c651cbd1ca050d6ab8b405ee
sunnyliuqi <liu963277983>
2019-03-04 16:50:33 +0800

    调用本地摄像头显示到页面 ok

--- app/src/main/java/com/example/webrtc_android/MainActivity.java
+++ app/src/main/java/com/example/webrtc_android/MainActivity.java
@@ -7,23 +7,99 @@ import android.view.View;
 
 import com.example.webrtc_android.websocket.WebSocketUtil;
 
+import org.webrtc.AudioSource;
+import org.webrtc.AudioTrack;
+import org.webrtc.Camera1Enumerator;
+import org.webrtc.EglBase;
+import org.webrtc.MediaConstraints;
+import org.webrtc.PeerConnectionFactory;
+import org.webrtc.SurfaceTextureHelper;
+import org.webrtc.SurfaceViewRenderer;
+import org.webrtc.VideoCapturer;
+import org.webrtc.VideoSource;
+import org.webrtc.VideoTrack;
+
+
 public class MainActivity extends AppCompatActivity {
-    WebSocketUtil ws = null;
+    private WebSocketUtil ws;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ws=WebSocketUtil.getInstance("ws://10.110.6.130:2019/chat/websocket");
+        ws = WebSocketUtil.getInstance("ws://10.110.6.130:2019/chat/websocket");
+
+        // create PeerConnectionFactory
+        PeerConnectionFactory.InitializationOptions initializationOptions =
+                PeerConnectionFactory.InitializationOptions.builder(this).createInitializationOptions();
+        PeerConnectionFactory.initialize(initializationOptions);
+        PeerConnectionFactory peerConnectionFactory = PeerConnectionFactory.builder().createPeerConnectionFactory();
+
+        // create AudioSource
+        AudioSource audioSource = peerConnectionFactory.createAudioSource(new MediaConstraints());
+        AudioTrack audioTrack = peerConnectionFactory.createAudioTrack("101", audioSource);
+
+        EglBase.Context eglBaseContext = EglBase.create().getEglBaseContext();
+
+        SurfaceTextureHelper surfaceTextureHelper = SurfaceTextureHelper.create("CaptureThread", eglBaseContext);
+        // create VideoCapturer
+        VideoCapturer videoCapturer = createCameraCapturer();
+        VideoSource videoSource = peerConnectionFactory.createVideoSource(videoCapturer.isScreencast());
+        videoCapturer.initialize(surfaceTextureHelper, getApplicationContext(), videoSource.getCapturerObserver());
+        videoCapturer.startCapture(480, 640, 30);
+
+        SurfaceViewRenderer localView = findViewById(R.id.localView);
+        localView.setMirror(true);
+        localView.init(eglBaseContext, null);
+
+        // create VideoTrack
+        VideoTrack videoTrack = peerConnectionFactory.createVideoTrack("101", videoSource);
+        // display in localView
+        videoTrack.addSink(localView);
+
     }
 
     public void send1(View view) {
-        ws.sendMsg("111","你好111");
+        ws.sendMsg("111", "你好111");
         Log.d("MainActivity", "呼叫111");
     }
 
     public void send2(View view) {
-        ws.sendMsg("222","你好222");
+        ws.sendMsg("222", "你好222");
         Log.d("MainActivity", "呼叫222");
+    }
+
+    public void permit(View view) {
+        Log.d("MainActivity", "同意");
+
+    }
+
+    private VideoCapturer createCameraCapturer() {
+        Camera1Enumerator enumerator = new Camera1Enumerator(false);
+        final String[] deviceNames = enumerator.getDeviceNames();
+
+        // First, try to find front facing camera
+        for (String deviceName : deviceNames) {
+            if (enumerator.isFrontFacing(deviceName)) {
+                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
+
+                if (videoCapturer != null) {
+                    return videoCapturer;
+                }
+            }
+        }
+
+        // Front facing camera not found, try something else
+        for (String deviceName : deviceNames) {
+            if (!enumerator.isFrontFacing(deviceName)) {
+                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
+
+                if (videoCapturer != null) {
+                    return videoCapturer;
+                }
+            }
+        }
+
+        return null;
     }
 }
---
+++ app/src/main/java/com/example/webrtc_android/webrtc/WebRtcUtil.java
@@ -0,0 +1,8 @@
+package com.example.webrtc_android.webrtc;
+
+/**
+ * webrtc 工具类
+ */
+public class WebRtcUtil {
+
+}
