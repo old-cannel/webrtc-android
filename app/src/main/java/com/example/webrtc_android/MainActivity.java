57c77fbff8fafbf7518bcf4430563ce7cd49ffee
sunnyliuqi <liu963277983>
2019-03-05 10:15:36 +0800

    页面添加远端视频加载窗口

--- app/src/main/java/com/example/webrtc_android/MainActivity.java
+++ app/src/main/java/com/example/webrtc_android/MainActivity.java
@@ -12,6 +12,8 @@ import org.webrtc.AudioTrack;
 import org.webrtc.Camera1Enumerator;
 import org.webrtc.EglBase;
 import org.webrtc.MediaConstraints;
+import org.webrtc.MediaStream;
+import org.webrtc.PeerConnection;
 import org.webrtc.PeerConnectionFactory;
 import org.webrtc.SurfaceTextureHelper;
 import org.webrtc.SurfaceViewRenderer;
@@ -22,40 +24,58 @@ import org.webrtc.VideoTrack;
 
 public class MainActivity extends AppCompatActivity {
     private WebSocketUtil ws;
-
+    PeerConnectionFactory peerConnectionFactory;
+    PeerConnection peerConnectionLocal;
+    PeerConnection peerConnectionRemote;
+    SurfaceViewRenderer localView;
+    SurfaceViewRenderer remoteView;
+    MediaStream mediaStreamLocal;
+    MediaStream mediaStreamRemote;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EglBase.Context eglBaseContext = EglBase.create().getEglBaseContext();
         ws = WebSocketUtil.getInstance("ws://10.110.6.130:2019/chat/websocket");
 
         // create PeerConnectionFactory
         PeerConnectionFactory.InitializationOptions initializationOptions =
                 PeerConnectionFactory.InitializationOptions.builder(this).createInitializationOptions();
         PeerConnectionFactory.initialize(initializationOptions);
-        PeerConnectionFactory peerConnectionFactory = PeerConnectionFactory.builder().createPeerConnectionFactory();
+        peerConnectionFactory = PeerConnectionFactory.builder().createPeerConnectionFactory();
+
 
         // create AudioSource
         AudioSource audioSource = peerConnectionFactory.createAudioSource(new MediaConstraints());
         AudioTrack audioTrack = peerConnectionFactory.createAudioTrack("101", audioSource);
 
-        EglBase.Context eglBaseContext = EglBase.create().getEglBaseContext();
 
+        // create videoSource
         SurfaceTextureHelper surfaceTextureHelper = SurfaceTextureHelper.create("CaptureThread", eglBaseContext);
         // create VideoCapturer
-        VideoCapturer videoCapturer = createCameraCapturer();
+        VideoCapturer videoCapturer = createCameraCapturer(true);
         VideoSource videoSource = peerConnectionFactory.createVideoSource(videoCapturer.isScreencast());
         videoCapturer.initialize(surfaceTextureHelper, getApplicationContext(), videoSource.getCapturerObserver());
         videoCapturer.startCapture(480, 640, 30);
 
+        // render to localView
         SurfaceViewRenderer localView = findViewById(R.id.localView);
         localView.setMirror(true);
         localView.init(eglBaseContext, null);
-
         // create VideoTrack
         VideoTrack videoTrack = peerConnectionFactory.createVideoTrack("101", videoSource);
         // display in localView
         videoTrack.addSink(localView);
+
+
+        // render to remoteView
+        SurfaceViewRenderer remoteView = findViewById(R.id.remoteView);
+        remoteView.setMirror(true);
+        remoteView.init(eglBaseContext, null);
+        // create VideoTrack
+        VideoTrack remoteVideoTrack = peerConnectionFactory.createVideoTrack("101", videoSource);
+        // display in remoteView
+        remoteVideoTrack.addSink(remoteView);
 
     }
 
@@ -74,24 +94,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private VideoCapturer createCameraCapturer() {
+    private VideoCapturer createCameraCapturer(boolean isFront) {
         Camera1Enumerator enumerator = new Camera1Enumerator(false);
         final String[] deviceNames = enumerator.getDeviceNames();
 
         // First, try to find front facing camera
         for (String deviceName : deviceNames) {
-            if (enumerator.isFrontFacing(deviceName)) {
-                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
-
-                if (videoCapturer != null) {
-                    return videoCapturer;
-                }
-            }
-        }
-
-        // Front facing camera not found, try something else
-        for (String deviceName : deviceNames) {
-            if (!enumerator.isFrontFacing(deviceName)) {
+            if (isFront ? enumerator.isFrontFacing(deviceName) : enumerator.isBackFacing(deviceName)) {
                 VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
 
                 if (videoCapturer != null) {
--- app/src/main/java/com/example/webrtc_android/webrtc/WebRtcUtil.java
+++
@@ -1,8 +0,0 @@
-package com.example.webrtc_android.webrtc;
-
-/**
- * webrtc 工具类
- */
-public class WebRtcUtil {
-
-}
