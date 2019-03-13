03b3e4c325839261a9f7d71ada8344b68cc461fa
sunnyliuqi <liu963277983>
2019-03-13 13:10:27 +0800

    blank

--- app/src/main/java/webrtc/example/com/webrtc_android/MainActivity.java
+++ app/src/main/java/webrtc/example/com/webrtc_android/MainActivity.java
@@ -1,13 +1,7 @@
- package webrtc.example.com.webrtc_android;
+package webrtc.example.com.webrtc_android;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
